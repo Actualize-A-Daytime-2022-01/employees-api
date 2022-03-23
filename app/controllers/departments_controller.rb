747ff00b966fe7bdda7461d5817fe6df9e3e83fc
William Meyer <williamjmiii@gmail.com>
2022-03-23 15:31:55 -0400

    adds department actions, and department requests

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,39 @@
 class DepartmentsController < ApplicationController
+  def index
+    departments = Department.all
+    render json: departments.as_json
+  end
+
+  def show
+    department = Department.find_by(id: params[:id])
+    render json: department.as_json
+  end
+
+  def create
+    department = Department.new(
+      department_name: params[:department_name],
+      manager: params[:manager],
+      code: params[:code]
+    )
+    department.save
+    render json: department.as_json
+  end
+
+  def update
+    department = Department.find_by(id: params[:id])
+    department.department_name = params[:department_name] || department.department_name
+    department.manager = params[:manager] || department.manager
+    department.code = params[:code] || department.code
+    
+    department.save
+    render json: department.as_json
+  end
+
+  def destroy
+    department = Department.find_by(id: params[:id])
+    department.destroy
+    render json: {message: "Department TAKEN CARE OF"}
+  end
+
 end
+
