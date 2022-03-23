3decf7d3f13dafa32cf2a0ae9020eccc40ebeebe
William Meyer <williamjmiii@gmail.com>
2022-03-23 15:18:21 -0400

    adds employee delete, employee update actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,10 +17,30 @@ class EmployeesController < ApplicationController
       phone: params[:phone],
       job_title: params[:job_title],
       department_id: params[:department_id],
-      admin: params[:admin]
+      admin: params[:admin] || false
     )
     employee.save
     render json: employee.as_json
+  end
+
+  def update
+    employee = Employee.find_by(id: params[:id])
+    employee.first_name = params[:first_name] || employee.first_name
+    employee.last_name = params[:last_name] || employee.last_name
+    employee.email = params[:email] || employee.email
+    employee.phone = params[:phone] || employee.phone
+    employee.job_title = params[:job_title] || employee.job_title
+    employee.department_id = params[:department_id] || employee.department_id
+    employee.admin = params[:admin] || false
+    
+    employee.save
+    render json: employee.as_json
+  end
+
+  def destroy
+    employee = Employee.find_by(id: params[:id])
+    employee.destroy
+    render json: {message: "Employee TAKEN CARE OF"}
   end
 
 end
