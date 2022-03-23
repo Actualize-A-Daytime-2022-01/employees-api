60b333476f329038af33a66a96e2bbf679b4013f
William Meyer <williamjmiii@gmail.com>
2022-03-23 14:04:03 -0400

    creates routes for employees and departments

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/employees" => "employees#index"
+  get "/employees/:id" => "employees#show"
+  post "/employees" => "employees#create"
+  patch "/employees/:id" => "employees#update"
+  delete "/employees/:id" => "employees#destroy"
+
+  get "/departments" => "departments#index"
+  get "/departments/:id" => "departments#show"
+  post "/departments" => "departments#create"
+  patch "/departments/:id" => "departments#update"
+  delete "/departments/:id" => "departments#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-  Employee.create(
-  first_name: "joe",
-  last_name: "schmoe",
-  email: "joe@example.com",
-  phone: "123-4567",
-  job_title: "Benefits Analyst",
-  department_id: 1
-)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+# #   Character.create(name: "Luke", movie: movies.first)
+#   Employee.create(
+#   first_name: "joe",
+#   last_name: "schmoe",
+#   email: "joe@example.com",
+#   phone: "123-4567",
+#   job_title: "Benefits Analyst",
+#   department_id: 1
+# )
 
-  Employee.create(
-  first_name: "jane",
-  last_name: "schmoe",
-  email: "jane@example.com",
-  phone: "113-4567",
-  job_title: "Benefits Coordinater",
-  department_id: 1,
-  admin: true
-)
+#   Employee.create(
+#   first_name: "jane",
+#   last_name: "schmoe",
+#   email: "jane@example.com",
+#   phone: "113-4567",
+#   job_title: "Benefits Coordinater",
+#   department_id: 1,
+#   admin: true
+# )
 
-  Department.create(
-  department_name: "HR",
-  manager: "Jane Schmoe",
-  code: "5000"
-)
+#   Department.create(
+#   department_name: "HR",
+#   manager: "Jane Schmoe",
+#   code: "5000"
+# )
 
