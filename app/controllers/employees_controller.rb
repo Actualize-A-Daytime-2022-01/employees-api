8034a7c545bce5d8f30fd91280fa48ddf4721dbc
William Meyer <williamjmiii@gmail.com>
2022-03-23 16:27:56 -0400

    updates employees controller to reflect views

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,16 @@
 class EmployeesController < ApplicationController
   def index
-    employees = Employee.all
-    render json: employees.as_json
+    @employees = Employee.all
+    render :index
   end
 
   def show
-    employee = Employee.find_by(id: params[:id])
-    render json: employee.as_json
+    @employee = Employee.find_by(id: params[:id])
+    render :show
   end
 
   def create
-    employee = Employee.new(
+    @employee = Employee.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
@@ -19,27 +19,27 @@ class EmployeesController < ApplicationController
       department_id: params[:department_id],
       admin: params[:admin] || false
     )
-    employee.save
-    render json: employee.as_json
+    @employee.save
+    render :show
   end
 
   def update
-    employee = Employee.find_by(id: params[:id])
-    employee.first_name = params[:first_name] || employee.first_name
-    employee.last_name = params[:last_name] || employee.last_name
-    employee.email = params[:email] || employee.email
-    employee.phone = params[:phone] || employee.phone
-    employee.job_title = params[:job_title] || employee.job_title
-    employee.department_id = params[:department_id] || employee.department_id
-    employee.admin = params[:admin] || false
+    @employee = Employee.find_by(id: params[:id])
+    @employee.first_name = params[:first_name] || @employee.first_name
+    @employee.last_name = params[:last_name] || @employee.last_name
+    @employee.email = params[:email] || @employee.email
+    @employee.phone = params[:phone] || @employee.phone
+    @employee.job_title = params[:job_title] || @employee.job_title
+    @employee.department_id = params[:department_id] || @employee.department_id
+    @employee.admin = params[:admin] || false
     
-    employee.save
-    render json: employee.as_json
+    @employee.save
+    render :show
   end
 
   def destroy
-    employee = Employee.find_by(id: params[:id])
-    employee.destroy
+    @employee = Employee.find_by(id: params[:id])
+    @employee.destroy
     render json: {message: "Employee TAKEN CARE OF"}
   end
 
